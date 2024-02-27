423252f749cd83767c5105c74de0bf799694faaa
Karthik-S-L-21 <karthik.sl@codeandtheory.com>
2024-02-27 18:32:30 +0530

    minor fix.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
+import { corsConfig } from './config/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors(corsConfig);
   app.use(cookieParser()); // cookie parser middleware
   await app.listen(process.env.PORT || 3000);
   console.log(`App Running on port ${process.env.PORT}`);
